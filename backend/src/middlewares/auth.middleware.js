60a9bc5f7b6b336d009702f1d18f8a7617b61ae5
Phalc0 _ <pierreintvg.pro@gmail.com>
2026-02-26 14:25:24 +0100

    changes on auth.middleware due to the cookie

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,13 +1,50 @@
+// V1
+// 
+//  const jwt = require("jsonwebtoken");
+// module.exports = (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+//     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+//     const userId = decodedToken.userId;
+//     req.auth = {
+//       userId: userId,
+//       role: decodedToken.role
+//     };
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: "Invalid token", error: error.message });
+//   }
+// };
+
+
+// V2 avec le cookie 
+
 const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    // Get the token from header Authorization or the cookie
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies && req.cookies.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
+      return res.status(401).json({ message: "Token manquant" });
+    }
+
+    // Verify the cookie
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
-    const userId = decodedToken.userId;
+
+    // stock user info into req
     req.auth = {
-      userId: userId,
-      role: decodedToken.role
+      userId: decodedToken.userId,
+      role: decodedToken.role,
     };
+
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid token", error: error.message });
